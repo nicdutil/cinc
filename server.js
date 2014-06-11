3fd1114bf3012d74d64a453a567f7a5f3f459f0b
drquantum <ledocteurquantum@gmail.com>
2014-06-11 03:54:49 -0400

    fix attempt #1

--- server.js
+++ server.js
@@ -3,22 +3,13 @@ var express = require('express'),
 
 app.use(express.logger());
 
-
 app.set('title', 'Cinc - Firme Informatique - Technologie Web - Minage - Visualisation');
-
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
-
-
-/*var server = app.listen(process.env.PORT, '192.168.1.6', function() { 
- console.log(__dirname);
- console.log('Express server started on port %s', process.env.PORT);
-});
-*/
 
 var server = app.listen(process.env.PORT, function() { 
  console.log(__dirname);
