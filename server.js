d4cfa0aee634b0309dfc85c206f7a6b66e27cbf4
infocinc <nicolas.dutil@infocinc.com>
2014-07-11 12:35:27 -0400

    add robots.txt to public folder enable gzip and cache control in server

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var express = require('express'),
 
 app.use(express.logger());
 
-app.set('title', 'Cinc - Firme Informatique - Technologie Web - Minage - Visualisation');
+app.set('title', 'CINC Conception Sites Webs Adaptatifs | Minage | Visualisation ');
+
+var oneDay = 86400000;
+
+app.use(express.compress());
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
-
-app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(process.env.PORT, function() { 
  console.log(__dirname);
