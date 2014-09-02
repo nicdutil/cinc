2b8a35588124a4a6938c05168512f6f7c2bb9d49
infocinc <nicolas.dutil@infocinc.com>
2014-09-02 18:37:14 -0400

    consolidate js and css files to reduce http requests

--- server-local.js
+++ server-local.js
@@ -1,9 +1,41 @@
 var express = require('express'),
     app = express();
+var compressor = require('node-minify');
+
+
+/*new compressor.minify({
+	type: 'yui-js',
+	fileIn: ['public/js/vendor/scrollTo.min.js','public/js/vendor/waypoints.min.js',
+			 'public/js/vendor/imagesloaded.pkgd.min.js','public/js/vendor/jquery.scrolldepth.min.js'],
+	fileOut: 'public/js-dist/base-min-yui.js',
+	callback: function(err,min) {
+		console.log(err);
+	}
+});
+*/
+new compressor.minify({
+	type: 'gcc',
+	fileIn: ['public/js/vendor/jquery.easing.1.3.js','public/js/vendor/bootstrap.js',
+			 'public/js/common.js','public/js/vendor/scrollTo.js','public/js/vendor/waypoints.js',
+			 'public/js/vendor/imagesloaded.pkgd.js','public/js/vendor/jquery.scrolldepth.js',
+			 'public/js/drops.js', 'public/js/tablet.js','public/js/init.js'],
+	fileOut: 'public/js-dist/base-min.js',
+	callback: function(err,min) {
+		console.log(err);
+	}
+});
+new compressor.minify({
+	type: 'clean-css',
+	fileIn: ['public/css/bootstrap.css','public/css/main.css'],
+	fileOut: 'public/css/base-min.css',
+	callback: function(err,min) {
+		console.log(err);
+	}
+});
 
 app.use(express.logger());
 
-app.set('title', 'CINC Conception Sites Webs | Minage | Visualisation ');
+app.set('title', 'Infocinc | Création Sites Webs | Minage | Visualisation ');
 
 var oneDay = 86400000;
 
