256b165ee95dc291b7f9bce7b960b862e15d0db6
infocinc <nicolas.dutil@infocinc.com>
2014-09-13 14:00:41 -0400

    fix border problem for links in banner

--- server-local.js
+++ server-local.js
@@ -1,36 +1,36 @@
+var https = require("https");
+var unirest = require('unirest');
+
+var loc_service = {
+    host: 'freegeoip.net',
+    path: '/json'
+}
+var mashape_key = "3hBdaIHGwpmshFPt6hqA18t1NIaqp1zW3Apjsnj8uzRLC2kcU1";
+
 var express = require('express'),
     app = express();
 var compressor = require('node-minify');
 
 
-/*new compressor.minify({
-	type: 'yui-js',
-	fileIn: ['public/js/vendor/scrollTo.min.js','public/js/vendor/waypoints.min.js',
-			 'public/js/vendor/imagesloaded.pkgd.min.js','public/js/vendor/jquery.scrolldepth.min.js'],
-	fileOut: 'public/js-dist/base-min-yui.js',
-	callback: function(err,min) {
-		console.log(err);
-	}
-});
-*/
 new compressor.minify({
-	type: 'gcc',
-	fileIn: ['public/js/vendor/jquery.easing.1.3.js','public/js/vendor/bootstrap.js',
-			 'public/js/common.js','public/js/vendor/scrollTo.js','public/js/vendor/waypoints.js',
-			 'public/js/vendor/jquery.scrolldepth.js',
-			 'public/js/drops.js', 'public/js/tablet.js','public/js/init.js'],
-	fileOut: 'public/js-dist/base-min.js',
-	callback: function(err,min) {
-		console.log(err);
-	}
+    type: 'gcc',
+    fileIn: ['public/js/vendor/jquery.easing.1.3.js', 'public/js/vendor/bootstrap.js',
+        'public/js/common.js', 'public/js/vendor/scrollTo.js', 'public/js/vendor/waypoints.js',
+        'public/js/vendor/jquery.scrolldepth.js',
+        'public/js/drops.js', 'public/js/tablet.js', 'public/js/init.js'
+    ],
+    fileOut: 'public/js-dist/base-min.js',
+    callback: function(err, min) {
+        console.log(err);
+    }
 });
 new compressor.minify({
-	type: 'clean-css',
-	fileIn: ['public/css/bootstrap.css','public/css/main.css'],
-	fileOut: 'public/css/base-min.css',
-	callback: function(err,min) {
-		console.log(err);
-	}
+    type: 'clean-css',
+    fileIn: ['public/css/bootstrap.css', 'public/css/main.css'],
+    fileOut: 'public/css/base-min.css',
+    callback: function(err, min) {
+        console.log(err);
+    }
 });
 
 app.use(express.logger());
@@ -42,14 +42,50 @@ var oneDay = 86400000;
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+function get_geoloc(ip, complete) {
+
+    unirest.get("https://worldtimeiofree.p.mashape.com/ip?ipaddress=" + '176.31.255.0')
+        .header("X-Mashape-Key", mashape_key)
+        .end(function(result) {
+        	var body = result.body;
+        	if (result.status != "200") {
+        		theme = 'day';
+        		console.log('ERROR WITH WORLD TIME SERVICE: ' + result.status);
+        	} else {
+			  var datetime = body['summary'].local;
+			  var time = datetime.split(' ')[1];
+			  var hour = parseInt(time.split(":")[0]);
+
+			  console.log('DATE TIME:' + datetime);
+			  console.log('HOUR:' + hour);
+			  if ((hour > 20) || (hour < 6)) {
+			  	theme = 'night';
+			  } else {
+			  	theme = 'day';
+			  }
+			  console.log('THEME:' + theme);
+			  complete(theme);
+        	}
+        });
+}
+
+
+app.get('/', function(req, res) {
+    var ip = req.headers['X-Forwarded-For'];
+    console.log('USER IP:' + ip);
     res.sendfile('index.html');
+/*    get_geoloc('202.13.34.255', function(theme) {
+    	switch(theme) {
+    		case 'day': 
+    		  res.sendfile('index.html');
+    		  break;
+    		case 'night':
+    		  res.sendfile('night.html'); 
+    	}
+    });
+*/});
+
+var server = app.listen(process.env.PORT, '192.168.1.6', function() {
+    console.log(__dirname);
+    console.log('Express server started on port %s', process.env.PORT);
 });
-
-var server = app.listen(process.env.PORT, '192.168.1.6', function() { 
- console.log(__dirname);
- console.log('Express server started on port %s', process.env.PORT);
-});
-
-
-
